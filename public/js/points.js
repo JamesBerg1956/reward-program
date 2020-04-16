14e240fdeaeb06556c6109eff05fbfef4a4373df
James Berg <james.berg38@yahoo.com>
2020-04-16 11:13:20 -0500

    added event listeners for number buttons and backspace button

--- public/js/points.js
+++ public/js/points.js
@@ -10,13 +10,23 @@ $(document).ready(function () {
 
   // TODO: generateRewardsHistoriesTable function
 
-  // START submit button event listener
+  // START number button event listener
   $(".number").click(function(){
-    // arrPhoneInput.push($(this).val());
-    // $(this).text(arrPhoneInput.join(''))
-    // console.log($(this).text);
+    arrPhoneInput.push($(this).val());
+    $("#phoneinput").text(arrPhoneInput.join(''))
   });
-  // END submit button event listener
+  // END number button event listener
+
+  // START backspace event listener
+  $("#button-backspace").click(function(){
+    arrPhoneInput.pop();
+    if(arrPhoneInput.length > 0){
+      $("#phoneinput").text(arrPhoneInput.join(''))
+    }
+    else{
+      $("#phoneinput").html("&nbsp;");
+    }
+  });
 
   // TODO: points submit button event listener
 
